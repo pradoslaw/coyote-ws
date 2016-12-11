23c5f833fc41314905454f4645f4308d83e0c06d
adam-boduch <adam@boduch.net>
2016-12-11 21:09:41 +0100

    klucz zapisany w base64

--- utils/crypt.py
+++ utils/crypt.py
@@ -7,17 +7,17 @@ from phpserialize import loads
 def decrypt(payload):
     data = json.loads(base64.b64decode(payload))
 
-    value =  base64.b64decode(data['value'])
+    value = base64.b64decode(data['value'])
     iv = base64.b64decode(data['iv'])
 
     return unserialize(mcrypt_decrypt(value, iv))
 
 
 def mcrypt_decrypt(value, iv):
-    AES.key_size=128
-    key=os.environ['APP_KEY']
+    AES.key_size = 128
+    key = base64.b64decode(os.environ['APP_KEY'])
 
-    crypt_object=AES.new(key=key, mode=AES.MODE_CBC, IV=iv)
+    crypt_object = AES.new(key=key, mode=AES.MODE_CBC, IV=iv)
 
     return crypt_object.decrypt(value)
 
