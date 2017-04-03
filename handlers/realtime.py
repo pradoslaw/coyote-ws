bd3e8773e8ab1f940357bf51a94de232bb527cf5
adam-boduch <adam@boduch.net>
2017-04-03 17:41:37 +0200

    komentarze

--- handlers/realtime.py
+++ handlers/realtime.py
@@ -67,19 +67,20 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
 
         self.is_connected = True
 
+        # cookie contains encypted session id
         cookie = self.get_cookie(os.environ['COOKIE'])
 
         if not cookie:
-            self.send_exit()
+            self._send_exit()
             return
 
-        # read session_id from cookie
+        # read cookie and decrypt it to obtain session id
         session_id = crypt.decrypt(urllib.unquote(cookie).decode('utf8'))
         # read payload from redis session
         payload = yield tornado.gen.Task(self.redis.hget, 'sessions', session_id)
 
         if payload is None:
-            self.send_exit()
+            self._send_exit()
         else:
             self.listen(payload)
 
@@ -94,36 +95,36 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
             data = loads(payload)
 
             self.channel = 'user:%d' % data['user_id'] if 'user_id' in data and data['user_id'] is not None else None
-
-            if self.channel:
-                logging.info('Client authenticated. Channel name: %s' % self.channel)
-
-                self.listener = tornadoredis.Client()
-                self.listener.connect()
-
-                yield tornado.gen.Task(self.listener.subscribe, self.channel)
-                self.listener.listen(self.on_event)
-
-            # send heartbeat after successful connection
-            self.send_heartbeat()
         except ValueError:
             logging.warning('Can not unserialize PHP object')
         except Exception as e:
-            logging.error(str(e))
+            logging.exception(e)
 
-    def send_heartbeat(self):
+        if self.channel:
+            logging.info('Client authenticated. Channel name: %s' % self.channel)
+
+            # send heartbeat after successful connection
+            self._send_heartbeat()
+
+            self.listener = tornadoredis.Client()
+            self.listener.connect()
+
+            yield tornado.gen.Task(self.listener.subscribe, self.channel)
+            self.listener.listen(self.on_event)
+
+    def _send_heartbeat(self):
         """
         Send heartbeat every 1 minute.
 
         :return:
         """
-        self._send_data({'event': 'hb', 'data': 'hb'})
-
         if self.is_connected:
-            # MUST BE send every 1 minutes so proxy (nginx) can keep connection alive
-            self.timeout_handler = tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(minutes=1), self.send_heartbeat)
+            self._send_data({'event': 'hb', 'data': 'hb'})
 
-    def send_exit(self):
+            # MUST BE send every 1 minutes so proxy (nginx) can keep connection alive
+            self.timeout_handler = tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(minutes=1), self._send_heartbeat)
+
+    def _send_exit(self):
         """
         Send exit event. WebSocket client WILL NOT attempt to reconnect.
 
@@ -139,7 +140,10 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
         :return:
         """
         try:
-            self.write_message(json.dumps(data))
+            json_data = json.dumps(data)
+
+            logging.info('Sending %s to %s' % (json_data, self.channel))
+            self.write_message(json_data)
         except tornado.websocket.WebSocketClosedError:
             logging.warning('Websocket closed when sending message.')
 
@@ -152,7 +156,7 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
         :param message:
         :return:
         """
-        logging.info('Message from websocket client: %s' % message)
+        logging.info('Message from websocket client : %s' % message)
 
     def on_event(self, message):
         """
@@ -183,7 +187,8 @@ class RealtimeHandler(tornado.websocket.WebSocketHandler):
             self.listener.disconnect()
 
             self.listener = None
-            self.channel = None
+
+        self.channel = None
 
         self.clients -= 1
         self.is_connected = False
