ce404d5c8e3627ce432001134bdfb71550864e7a
adam <adam@boduch.net>
2019-12-30 17:08:49 +0100

    odczytywanie pola z docker secret

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
 # settings.py
 from os.path import join, dirname
+from os import environ
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+try:
+    with open('/run/secrets/APP_KEY', 'r') as secret_file:
+        environ['APP_KEY'] = secret_file.read()[7:]
+except IOError:
+    pass
