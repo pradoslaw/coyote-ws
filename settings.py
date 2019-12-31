d4ecef36d22a3212b813c2a965accba4f7513021
adam <adam@boduch.net>
2019-12-31 10:12:28 +0100

    pomylka w nazwie pliku

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 try:
-    with open('/run/secrets/APP_KEY', 'r') as secret_file:
+    with open('/run/secrets/APP_KEY_FILE', 'r') as secret_file:
         environ['APP_KEY'] = secret_file.read()[7:]
 except IOError:
     pass
