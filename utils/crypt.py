64af66be6ee0e58b9109beb0a24ec35f41a88588
adam <adam@boduch.net>
2020-12-20 13:36:07 +0100

    test

--- utils/crypt.py
+++ utils/crypt.py
@@ -2,8 +2,9 @@ import os
 import jwt
 
 def jwt_decode(token):
+    print('Key: %s' % os.environ['APP_KEY'])
     payload = jwt.decode(token, os.environ['APP_KEY'], algorithms=['HS256'])
-    print(payload)
+
     return payload
 
 
