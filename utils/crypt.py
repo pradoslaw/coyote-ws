cc9c80a930e77bafac07009a61f0b7a47cd6eab8
adam <adam@boduch.net>
2020-12-21 18:48:01 +0100

    usuniety zbedny kod

--- utils/crypt.py
+++ utils/crypt.py
@@ -2,7 +2,6 @@ import os
 import jwt
 
 def jwt_decode(token):
-    print('Key: %s' % os.environ['APP_KEY'])
     payload = jwt.decode(token, os.environ['APP_KEY'], algorithms=['HS256'])
 
     return payload
